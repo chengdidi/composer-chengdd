666021f21e4c8d3049f8ec21c3ebb2a0fda4c2b0
CDD <chengdidi@ibjcr.cn>
2021-06-23 08:59:19 +0800

    测试composer

---
+++ src/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CDD
+ * Date: 2021/6/23
+ * Time: 8:57
+ */
+
+namespace Hfcfb\Chengdd;
+
+
+class Test
+{
+    public function hiha()
+    {
+        echo '666', PHP_EOL;
+    }
+}
